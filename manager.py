8dbf4411b7138c25cd6ccb01db2f7922d8c874e8
Nikita Juyal <32689770+juyalnikita@users.noreply.github.com>
2017-12-11 18:07:25 +0000

    manager.py

---
+++ manager.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+flask = Flask(__name__)
+api = Api(app)
+
+#class getReposit(Resource)
+def __init__(self):
+    global managerS 
+    self.server = managerS #initialise the global server
+
+
+#class cyclomaticApi() to obtain commits and post cyclometric complexity results
+def __init__(self):
+    global managerS 
+    self.server = managerS #initialise the global server
+
+#class manager(): to be defined
+
+if __name__ == "__main__":
+    managerS = manager()  # initialise instance of managerServer()
+    app.run(port=5000) 
