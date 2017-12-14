ca579b71ef9832a3010a8a3fbb36303c272b5b1f
Nikita Juyal <32689770+juyalnikita@users.noreply.github.com>
2017-12-14 19:06:04 +0000

    Final updates for Worker.py

--- Worker.py
+++ Worker.py
@@ -20,8 +20,32 @@ def run():
      #Repo pulled
      r = requests.get("http://{}:{}/repo".format(ipManager,portManager),json = {'pullStatus' : True})
      
-     
-     #add code for multiple commits
+     stillHaveCommits = True
+     while stillHaveCommits:
+     r = requests.get("http://{}:{}/cyclomatic".format(ipManager,portManager))
+     json_data = json.loads(r.text)
+        print(json_data)
+        print("Received : {}".format(json_data['sha']))
+        if json_data['sha'] == -2:  #  Manager waiting to start giving commits
+           print("Manager waiting")
+        else:
+           if json_data['sha'] == -1:
+                print("No items left")
+                break
+              subprocess.call(["bash", "workerGetCommit.sh", json_data['sha']])
+              binRadonCCOutput = subprocess.check_output(["radon", "cc", "-s", "-a" , "workerData"])
+              radonCCOutput = binRadonCCOutput.decode("utf-8")
+           
+           print(radonCCOutput)
+           avgCCstartPos = radonCCOutput.rfind("(") # lookign for last bracket
+           if radonCCOutput[avgCCstartPos+1:-2] == "": #no files left to calculate complexity
+             print("No files")
+             r = requests.post("http://{}:{}/cyclomatic".format(ipManager,portManager),json={'commitSha': json_data['sha'], 'complexity': -1})
+        
+        else:
+          averageCC = float(radonCCOutput[avgCCstartPos+1:-2]) #get avg Cyclometric Complexity
+          r = requests.post("http://{}:{}/cyclomatic".format(ipManager,portManager), json={'commitSha': json_data['sha'], 'complexity': averageCC})
+       numCommitsDone += 1
        
 if __name__ == '__main__':
       run()
